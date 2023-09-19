2eb5beaf1e221f5fb00f91d18e81bb62f754c279
Enrico Zandomeni Borba <enricozb@gmail.com>
2023-09-18 22:30:50 -1000

    nut runs

--- flake.nix
+++ flake.nix
@@ -2,12 +2,20 @@
   description = "nut";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nut.url = "github:blawar/nut";
+  inputs.nut = {
+    url = "github:blawar/nut";
+    flake = false;
+  };
 
   outputs = { self, nixpkgs, flake-utils, nut }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        python = (pkgs.python311.withPackages python-pkgs);
+        nut-bin = pkgs.writeShellScriptBin "nut" ''
+          QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins" \
+          ${python}/bin/python ${nut}/nut_gui.py
+        '';
         python-pkgs = (ps:
           with ps; [
             asn1
@@ -15,34 +23,50 @@
             certifi
             colorama
             filelock
-            Flask
+            flask
             future
             google-api-python-client
             google-auth-oauthlib
             humanize
             markupsafe
-            Pillow
+            pillow
             pycryptodome
-            pycryptoplus
+            (buildPythonPackage rec {
+              pname = "pycryptoplus";
+              version = "0.0.1";
+              src = fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-BnBAuUgr1bj3jABJIjwsL+ogtOcnUe6CqelX+bQyypQ=";
+              };
+              propagatedBuildInputs = [ pkgs.python311Packages.pycryptodome ];
+            })
             pycurl
             pyopenssl
             pyqt5
             pyusb
-            qt-range-slider
+            (buildPythonPackage rec {
+              pname = "qt-range-slider";
+              version = "0.2.7";
+              format = "pyproject";
+              src = fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-KcEWFAaxRy/aXdW05p1PRn1vqnAjFEsqCiPYgqVsNM8=";
+              };
+              propagatedBuildInputs = with pkgs.python311Packages; [
+                pyqt6
+                pyqt6-sip
+              ];
+            })
             requests
             tqdm
-            Unidecode
+            unidecode
             urllib3
             watchdog
             zstandard
           ]);
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            curl
-            libusb
-            (python39.withPackages python-pkgs)
-          ];
+          packages = with pkgs; [ curl libusb python nut-bin ];
         };
       });
 }
